182325e581f669932da43643b86f28ef37f8c66b
frankzhang <frankzhang945@gmail.com>
2016-07-21 15:55:48 -0400

    Hello World

---
+++ helloworld.py
@@ -0,0 +1,18 @@
+"""knowledge 
+1)https://docs.python.org/3/howto/pyporting.html
+Porting Python 2 Code to Python 3
+2)	urllib
+	urllib2# in python3 there is no urllib2
+	httplib
+	httplib2
+	Beautiful Soup https://www.crummy.com/software/
+	lxml
+	GTQL
+	Scrapy
+	Mechanize
+	PyQuery
+	requests docs.pythin-request.org/en/latest/
+	selenium #test simulate clicking
+	
+	ghost.py
+	
